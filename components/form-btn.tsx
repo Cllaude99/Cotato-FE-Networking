63e590581f7ed2784d9b3363350af1927348bfed
Cllaude99 <xodbsrla1025@naver.com>
2024-04-23 18:14:41 +0900

    💄 Design: 버튼 컴포넌트 수정

--- components/form-btn.tsx
+++ components/form-btn.tsx
@@ -9,7 +9,7 @@ export default function FormButton({ loading, text }: IFormButton) {
       className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
       disabled={loading}
     >
-      {loading ? '확인 중...' : text}
+      {loading ? '잠시만 기다려 주세요...' : text}
     </button>
   );
 }
