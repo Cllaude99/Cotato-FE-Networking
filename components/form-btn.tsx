55fdb6db7be231efb0ed729642bf1a1ee7cc296a
Cllaude99 <xodbsrla1025@naver.com>
2024-04-23 17:51:15 +0900

    💄 Design: 회원가입, 로그인 화면 UI

---
+++ components/form-btn.tsx
@@ -0,0 +1,15 @@
+interface IFormButton {
+  loading: boolean;
+  text: string;
+}
+
+export default function FormButton({ loading, text }: IFormButton) {
+  return (
+    <button
+      className="primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed"
+      disabled={loading}
+    >
+      {loading ? '확인 중...' : text}
+    </button>
+  );
+}
