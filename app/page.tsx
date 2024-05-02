bacb6701d3bc42871ffbe037d981c80a3fd496d8
Cllaude99 <xodbsrla1025@naver.com>
2024-05-02 20:16:13 +0900

    💄 styles: main 페이지 UI 수정

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-        <span className="text-9xl">🥔</span>
+        <span className="text-9xl"></span>
         <h1 className="text-4xl">코테이토</h1>
         <h2 className="text-2xl">Cotato FE Networking</h2>
       </div>
