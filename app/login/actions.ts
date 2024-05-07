17affff395ddc061e2d9871662a36243982d3691
Cllaude99 <xodbsrla1025@naver.com>
2024-05-08 03:31:08 +0900

    ✨ feat: 로그인 페이지 구현

---
+++ app/login/actions.ts
@@ -0,0 +1,90 @@
+'use server';
+
+import {
+  EMAIL_REGEX,
+  EMAIL_REGEX_ERROR,
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
+  PASSWORD_REQUIRED_ERROR,
+} from '@/lib/constants';
+import db from '@/lib/db';
+import getSession from '@/lib/session';
+import bcrypt from 'bcrypt';
+import { redirect } from 'next/navigation';
+
+import { z } from 'zod';
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return Boolean(user);
+};
+const formSchema = z.object({
+  email: z
+    .string()
+    .toLowerCase()
+    .email()
+    .regex(EMAIL_REGEX, EMAIL_REGEX_ERROR)
+    .refine(checkEmailExists, '해당 이메일을 가진 유저가 존재하지 않습니다 😅'),
+  password: z
+    .string({
+      required_error: PASSWORD_REQUIRED_ERROR,
+    })
+    .min(PASSWORD_MIN_LENGTH)
+    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+});
+
+export const handleLogin = async (prevState: any, formData: FormData) => {
+  const data = {
+    email: formData.get('email'),
+    password: formData.get('password'),
+  };
+
+  const result = await formSchema.safeParseAsync(data);
+
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    // 입력한 email을 가진 사용자
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+
+    // 입력한 비밀번호가 기존 비밀번호와 일치한지에 대한 여부 확인
+    const ok = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? 'xxx'
+    );
+
+    // 비밀번호가 일치하는 경우
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+      await session.save();
+
+      redirect('/profile');
+    } else {
+      // 비밀번호가 일치하지 않는 경우
+      return {
+        fieldErrors: {
+          email: [],
+          password: ['잘못된 비밀번호 입니다 😅'],
+        },
+      };
+    }
+  }
+};
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,38 @@
-import FormButton from '@/components/form-btn';
-import FormInput from '@/components/form-input';
+'use client';
+
+import Button from '@/components/button';
+import Input from '@/components/input';
 import SocialLogin from '@/components/social-login';
+import { useFormState } from 'react-dom';
+import { handleLogin } from './actions';
+import { PASSWORD_MIN_LENGTH } from '@/lib/constants';
 
 export default function Login() {
+  const [state, formAction] = useFormState(handleLogin, null);
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" errors={[]} required />
-        <FormInput
-          type="password"
-          placeholder="Password"
-          errors={[]}
+      <form action={formAction} className="flex flex-col gap-3">
+        <Input
+          name="email"
+          type="email"
+          placeholder="이메일을 입력해주세요"
+          errors={state?.fieldErrors.email}
           required
         />
-        <FormButton loading={false} text="Create account" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
+          required
+        />
+        <Button text="로그인" />
       </form>
       <SocialLogin />
     </div>
