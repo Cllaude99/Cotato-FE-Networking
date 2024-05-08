cdd02e697a8cbfebc217d4c9d5b0e7695dd0a46f
Cllaude99 <xodbsrla1025@naver.com>
2024-05-08 15:27:25 +0900

    design: input 색상 수정

--- components/input.tsx
+++ components/input.tsx
@@ -14,7 +14,7 @@ export default function Input({
     <div className="flex flex-col gap-2">
       <input
         name={name}
-        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 transition"
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 ring-neutral-200 focus:ring-blue-400 border-none placeholder:text-neutral-400 transition"
         {...restProps}
       />
       {errors.map((error, index) => (
