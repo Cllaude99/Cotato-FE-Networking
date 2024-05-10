466aadefd866be4ca947a8e411ef792e1cb53aab
Cllaude99 <xodbsrla1025@naver.com>
2024-05-11 00:59:21 +0900

    feat: 깃허브 로그인 구현

--- app/social-login/github/callback/route.ts
+++ app/social-login/github/callback/route.ts
@@ -55,7 +55,7 @@ export async function GET(request: NextRequest) {
 
   if (user) {
     await updateSession(user.id);
-    return redirect('/profile');
+    return redirect('/networking');
   }
 
   const newUser = await db.user.create({
@@ -69,5 +69,5 @@ export async function GET(request: NextRequest) {
     },
   });
   await updateSession(newUser.id);
-  return redirect('/profile');
+  return redirect('/networking');
 }
