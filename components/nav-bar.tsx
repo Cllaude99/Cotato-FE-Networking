c729b39927e7e3a25b6e680eae6fe8697ad387eb
Cllaude99 <xodbsrla1025@naver.com>
2024-05-14 05:52:28 +0900

    feat: navbar 컴포넌트

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -8,12 +8,12 @@ export default function NavBar() {
     <div className="w-full mx-auto max-w-screen-lg grid grid-cols-5 border-neutral-600 border-b px-5 py-3 *:text-white *:text-lg my-3 ">
       <Link href="/products" className="flex flex-col items-center gap-px">
         <span className={pathname === '/products' ? 'text-blue-400' : ''}>
-          홈
+          네트워킹
         </span>
       </Link>
       <Link href="/life" className="flex flex-col items-center gap-px">
         <span className={pathname === '/life' ? 'text-blue-400' : ''}>
-          동네생활
+          몰라
         </span>
       </Link>
       <Link href="/chat" className="flex flex-col items-center gap-px">
@@ -23,12 +23,12 @@ export default function NavBar() {
       </Link>
       <Link href="/live" className="flex flex-col items-center gap-px">
         <span className={pathname === '/live' ? 'text-blue-400' : ''}>
-          쇼핑
+          공유
         </span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-px">
         <span className={pathname === '/profile' ? 'text-blue-400' : ''}>
-          나의 당근
+          프로필
         </span>
       </Link>
     </div>
