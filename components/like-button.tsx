b6367a7f2224898e2539afb0bd80f43ef65e291a
Cllaude99 <xodbsrla1025@naver.com>
2024-06-12 04:20:02 +0900

    refactor: 좋아요 버튼 컴포넌트 분리

---
+++ components/like-button.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { HandThumbUpIcon } from '@heroicons/react/24/solid';
+import { HandThumbUpIcon as OutlineHandThumbUpIcon } from '@heroicons/react/24/outline';
+import { useOptimistic } from 'react';
+import { dislikePost, likePost } from '@/app/(tabs)/posts/[id]/actions';
+
+interface LikeButtonProps {
+  isLiked: boolean;
+  likeCount: number;
+  postId: number;
+}
+
+export default function LikeButton({
+  isLiked,
+  likeCount,
+  postId,
+}: LikeButtonProps) {
+  const [state, reducerFn] = useOptimistic(
+    { isLiked, likeCount },
+    (previousState, payload) => ({
+      isLiked: !previousState.isLiked,
+      likeCount: previousState.isLiked
+        ? previousState.likeCount - 1
+        : previousState.likeCount + 1,
+    })
+  );
+  const onClick = async () => {
+    reducerFn(undefined);
+    if (isLiked) {
+      await dislikePost(postId);
+    } else {
+      await likePost(postId);
+    }
+  };
+  return (
+    <button
+      onClick={onClick}
+      className={`flex items-center gap-2 ml-5 text-neutral-400 text-sm border border-neutral-400 rounded-3xl p-2  transition-colors ${
+        state.isLiked && 'bg-blue-500 text-white border-none'
+      }`}
+    >
+      {state.isLiked ? (
+        <HandThumbUpIcon className="size-5" />
+      ) : (
+        <OutlineHandThumbUpIcon className="size-5" />
+      )}
+      {state.isLiked ? <span> {state.likeCount}</span> : <span>좋아요</span>}
+    </button>
+  );
+}
