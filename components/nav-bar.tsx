7499dbe4027cd9793b43c64e39221657a6bba2e2
Cllaude99 <xodbsrla1025@naver.com>
2024-06-16 16:35:16 +0900

    feat: 메뉴 수정

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -6,16 +6,6 @@ export default function NavBar() {
   const pathname = usePathname();
   return (
     <div className="w-full mx-auto max-w-screen-lg grid grid-cols-5 border-neutral-600 border-b px-5 py-3 *:text-white *:text-lg my-3 ">
-      <Link href="/products" className="flex flex-col items-center gap-px">
-        <span className={pathname === '/products' ? 'text-blue-400' : ''}>
-          네트워킹
-        </span>
-      </Link>
-      <Link href="/life" className="flex flex-col items-center gap-px">
-        <span className={pathname === '/life' ? 'text-blue-400' : ''}>
-          에러해결
-        </span>
-      </Link>
       <Link href="/chat" className="flex flex-col items-center gap-px">
         <span className={pathname === '/chat' ? 'text-blue-400' : ''}>
           질문
@@ -24,6 +14,16 @@ export default function NavBar() {
       <Link href="/live" className="flex flex-col items-center gap-px">
         <span className={pathname === '/live' ? 'text-blue-400' : ''}>
           공유
+        </span>
+      </Link>
+      <Link href="/products" className="flex flex-col items-center gap-px">
+        <span className={pathname === '/products' ? 'text-blue-400' : ''}>
+          Colog
+        </span>
+      </Link>
+      <Link href="/faq" className="flex flex-col items-center gap-px">
+        <span className={pathname === '/faq' ? 'text-blue-400' : ''}>
+          과제 FAQ
         </span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-px">
